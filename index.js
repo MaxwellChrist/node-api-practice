c26b837209869f6e19ccb559e647f85c412377fe
MaxwellChrist <ribthulu@gmail.com>
2022-05-12 14:00:39 -0400

    final

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 
-const server = require('./api/server')
+const server = require('./api/server.js');
 
-const port = process.env.PORT || 9000;
+const PORT = process.env.PORT;
 
-server.listen(port , () => {
-    console.log(`server listening on port ${port}`);
-})
+if(PORT == null) {
+    console.error('no port set');
+} else {
+    server.listen(PORT, () => {
+        console.log(`server is running on port ${PORT}`);
+        console.log(`environment is ${process.env.NODE_ENV}`);
+    });
+}
