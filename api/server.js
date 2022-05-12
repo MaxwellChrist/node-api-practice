c453d44057d82f897df807c890b7c63fec03db56
MaxwellChrist <ribthulu@gmail.com>
2022-05-12 13:43:28 -0400

    set up fortunes and stuff model

--- api/server.js
+++ api/server.js
@@ -1,13 +1,50 @@
 const express = require('express');
+const fortunes = require('../fortunes');
+const Stuff = require('./stuff-model');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
-    res.send('<h1>Hello world!</h1>')
-})
+    res.send('<h1>HEROKU DEPLOYMENT!!</h1>');
+});
 
 server.get('/node-env', (req, res) => {
-    res.json({ something: "Hello!" });
-})
+    res.json({ NODE_ENV: process.env.NODE_ENV });
+});
 
-module.exports = server
+server.get('/fortune', (req, res) => {
+    let randomIndex = Math.floor(Math.random() * fortunes.length);
+    res.send(`<h1>${fortunes[randomIndex]}</h1>`);
+});
+
+
+
+server.get('/stuff', (req, res) => {
+    Stuff.getAll()
+        .then(result => {
+            res.json(result);
+        });
+});
+
+server.get('/stuff/:index', (req, res) => {
+    Stuff.getByIndex(req.params.index)
+        .then(result => {
+            if(result == null) {
+                res.status(404).json({ message: 'not found' });
+                return;
+            }
+            res.json(result);
+        });
+});
+
+server.post('/stuff', (req, res) => {
+    Stuff.add(req.body)
+        .then(result => {
+            res.status(201).json(result);
+        });
+});
+
+
+module.exports = server;
